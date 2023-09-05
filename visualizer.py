776a571a874244645da176346f4460ba55e4d722
August Hedman <augustlh@icloud.com>
2023-09-05 15:53:33 +0200

    neh

--- visualizer.py
+++ visualizer.py
@@ -3,7 +3,14 @@ import pygame, random
 
 class Visualizer:
     def __init__(self, title, size):
-        pass
+        pygame.init()
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock()
+        
 
     def animate_bars(self, tickrate):
+        pass
+
+    def draw_bars(self):
         pass
