ee6f313577f957c8ac8a6fc33bc2a33ab7828c2e
August Hedman <augustlh@icloud.com>
2023-09-25 14:33:48 +0200

    qe

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from sorting_algorithms import bubble_sort, selection_sort, insertion_sort, gene
 from values import n
 
 visualizer = Visualizer("Visualisering af sorterings algoritmer")
-#visualizer.bar_animation(mergeSort, generate_data(n, type="bar"))
-#visualizer.await_keypress('c')
+visualizer.bar_animation(mergeSort, generate_data(n, type="bar"))
+visualizer.await_keypress('c')
 #visualizer.bar_animation(augussySort, generate_data(n))
 #visualizer.await_keypress('c')
 #visualizer.bar_animation(bubble_sort, generate_data(n, type="bar"))
 #visualizer.await_keypress('c')
-#visualizer.bar_animation(selection_sort, generate_data(size=n, type="bar"))
-#visualizer.await_keypress('c')
-#visualizer.bar_animation(insertion_sort, generate_data(size=n, type="bar"))
-#visualizer.await_keypress('c')
+visualizer.bar_animation(selection_sort, generate_data(size=n, type="bar"))
+visualizer.await_keypress('c')
+visualizer.bar_animation(insertion_sort, generate_data(size=n, type="bar"))
+visualizer.await_keypress('c')
 
 #box animations
+visualizer.box_animation(bubble_sort, generate_data(size=10, type="box"))
+visualizer.await_closure()
+visualizer.box_animation(selection_sort, generate_data(size=10, type="box"))
+visualizer.await_closure()
 visualizer.box_animation(insertion_sort, generate_data(size=10, type="box"))
 visualizer.await_closure()
